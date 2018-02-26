c76a43861762cb03f60b04c2b5b461d25ef0295e
Luis Carlin <luiscarlin@gmail.com>
2018-02-26 06:51:03 -0500

    Setup Enzyme and write button with first test

---
+++ src/presenters/Button/Button.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class Button extends React.Component {
+  render() {
+    return (
+      <div>
+        <button>
+          click here
+      </button>
+      </div>
+    )
+  }
+}
+
+export default Button
