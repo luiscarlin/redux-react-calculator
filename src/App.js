7d2ff37764e2483399fc7225918f0c8996492990
Luis Carlin <luiscarlin@gmail.com>
2018-02-27 07:04:31 -0500

    WIP2 - Store working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
+import configureStore from './store/configureStore'
 import Button from './presenters/Button'
+import { Provider } from 'react-redux'
+
+const store = configureStore();
 
 class App extends Component {
+  
   render() {
     return (
-      <Button display='1'/>
+      <Provider store={store}>
+        <Button display='1' />
+      </Provider>
     )
   }
 }
