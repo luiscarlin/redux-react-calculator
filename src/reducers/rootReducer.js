ce32404abdaa0b86ba0b132052fd74e57cc429af
Luis Carlin <luiscarlin@gmail.com>
2018-03-03 18:19:18 -0500

    fix path

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux'
-import displayReducer from './displayReducer'
+import displayReducer from './display'
 
 const rootReducer = combineReducers({
   displayReducer
