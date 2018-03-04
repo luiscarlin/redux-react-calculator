2b59b586297df33ba9ae7748396d63c2939226fa
Luis Carlin <luiscarlin@gmail.com>
2018-03-04 16:18:52 -0500

    add more display reducer tests

--- src/reducers/display/displayReducer.test.js
+++ src/reducers/display/displayReducer.test.js
@@ -1,11 +1,20 @@
 import displayReducer from './'
 
 describe('Display Reducer', () => {
+  const INITIAL_STATE = {
+    display: ''
+  }
+
   it('should return original state when cannot handle action', () => {
     let originalState = {
       display: 'something'
     }
-    let nextState = displayReducer(originalState, { type: "what", value: "do you want" })
+    const nextState = displayReducer(originalState, { type: "what", value: "do you want" })
     expect(nextState).toEqual(originalState)
-  })  
+  })
+
+  it('should use the initial state as default', () => {
+    const state = displayReducer(undefined, { type: "hello" })
+    expect(state).toEqual(INITIAL_STATE)
+  })
 })
