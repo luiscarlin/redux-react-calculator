8e43ce7dc07ab6e1d7b8a71b2783236032ed7624
Luis Carlin <luiscarlin@gmail.com>
2018-03-04 16:43:08 -0500

    should only update display field in new state

--- src/reducers/display/displayReducer.test.js
+++ src/reducers/display/displayReducer.test.js
@@ -1,20 +1,32 @@
 import displayReducer from './'
+import { PUSH_TO_DISPLAY } from '../../actions';
 
 describe('Display Reducer', () => {
   const INITIAL_STATE = {
     display: ''
   }
 
-  it('should return original state when cannot handle action', () => {
-    let originalState = {
+  it('returns the current state when cannot handle action', () => {
+    let currentState = {
       display: 'something'
     }
-    const nextState = displayReducer(originalState, { type: "what", value: "do you want" })
-    expect(nextState).toEqual(originalState)
+    const nextState = displayReducer(currentState, { type: "what", value: "do you want" })
+    expect(nextState).toEqual(currentState)
   })
 
   it('should use the initial state as default', () => {
     const state = displayReducer(undefined, { type: "hello" })
     expect(state).toEqual(INITIAL_STATE)
+  })
+
+  describe('when given the action to push to display', () => {
+    
+    it('returns a new state with display field updated', () => {
+      const currentState = {
+        display: '123'
+      }
+      const nextState = displayReducer(currentState, { type: PUSH_TO_DISPLAY, value: '4' })
+      expect(nextState.display).toEqual('1234')
+    })
   })
 })
