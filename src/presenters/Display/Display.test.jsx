a6b39c5df68b210ba951bbdbfc7f30fe1be4e4fe
Luis Carlin <luiscarlin@gmail.com>
2018-03-05 07:04:02 -0500

    accepts value from property

--- src/presenters/Display/Display.test.jsx
+++ src/presenters/Display/Display.test.jsx
@@ -1,8 +1,8 @@
 import Display from './' 
 
 describe('Display', () => {
-  it('renders an input field', () => {
-    const display = shallow(<Display />)
-    expect(display.find('input')).toHaveLength(1)
+  it('renders an input field with value accepted from a property', () => {
+    const display = shallow(<Display value='funny' />)
+    expect(display.find('input').props().value).toEqual('funny')
   })
 })
