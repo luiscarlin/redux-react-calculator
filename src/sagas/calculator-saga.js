3d97eb36ee1a5e4b0d04b3600ea1dbf7516407e4
Luis Carlin <luiscarlin@gmail.com>
2018-03-14 09:22:30 -0400

    Output message after calculating

--- src/sagas/calculator-saga.js
+++ src/sagas/calculator-saga.js
@@ -2,9 +2,9 @@ import { takeEvery, put } from 'redux-saga/effects'
 import { CALCULATE } from '../actions/actionTypes'
 import { pushToDisplay, clearDisplay } from '../actions/displayActions'
 
-
 function * calculate(action) {
   yield put(clearDisplay())
+  yield(put(pushToDisplay("hello from the other side")))
 }
 
 export default function * calculatorSaga() {
