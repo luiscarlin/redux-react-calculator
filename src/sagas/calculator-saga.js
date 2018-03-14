97b4122217fe55cd1310401e561f37841be74eb0
Luis Carlin <luiscarlin@gmail.com>
2018-03-14 09:25:24 -0400

    Added next steps as comments

--- src/sagas/calculator-saga.js
+++ src/sagas/calculator-saga.js
@@ -3,8 +3,14 @@ import { CALCULATE } from '../actions/actionTypes'
 import { pushToDisplay, clearDisplay } from '../actions/displayActions'
 
 function * calculate(action) {
+  // pull the current value
+  // calculate the actual result 
+  // if success 
   yield put(clearDisplay())
-  yield(put(pushToDisplay("hello from the other side")))
+  yield put(pushToDisplay("hello from the other side"))
+
+  // if failure
+  // send an error action to the reducer
 }
 
 export default function * calculatorSaga() {
