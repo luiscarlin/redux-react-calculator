a8422e6695c389898bb32e9e7a3c18b84c86dd32
Luis Carlin <luiscarlin@gmail.com>
2018-03-20 08:38:00 -0400

    Fix linter issue and start css

--- src/presenters/Button/Button.test.jsx
+++ src/presenters/Button/Button.test.jsx
@@ -5,10 +5,10 @@ describe('Button', () => {
     const subject = shallow(<Button value={'1234'}/>)
     expect(subject.find('button').text()).toEqual('1234')
   })
-  
+
   it('executes onclick function when clicked', () => {
     const onClickMock = jest.fn()
-    const subject = shallow(<Button onClick={onClickMock} value={"something"} />)
+    const subject = shallow(<Button onClick={onClickMock} value={'something'} />)
     subject.find('button').simulate('click')
     expect(onClickMock).toHaveBeenCalledWith('something')
   })
